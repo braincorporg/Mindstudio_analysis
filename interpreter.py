c67a0d3d62858fc93755d96268fc4bd7ea001366
Braingearceo <104493450+Braingearceo@users.noreply.github.com>
2023-11-18 12:08:56 +0100

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -1,38 +1,81 @@
-from fastapi import FastAPI, HTTPException
-import requests
+# from fastapi import FastAPI, HTTPException
+# import requests
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import base64
+# from io import BytesIO
+
+# app = FastAPI()
+
+# @app.post("/process_excel/")
+# async def process_excel(url: str):
+#     # Download the Excel file
+#     try:
+#         response = requests.get(url)
+#         response.raise_for_status()  # This will raise an error for bad responses
+#     except requests.RequestException as e:
+#         raise HTTPException(status_code=400, detail=str(e))
+
+#     # Read the Excel file into a DataFrame
+#     df = pd.read_excel(BytesIO(response.content))
+
+#     # Generate a graph and convert it to base64
+#     graph_base64 = generate_graph_base64(df)
+
+#     # Return 'Hello World' and the graph
+#     return {"message": "Hello World", "graph": graph_base64}
+
+# def generate_graph_base64(dataframe):
+#     plt.figure()
+#     dataframe.plot(kind='line')  # Customize this as per your data
+#     plt.tight_layout()
+
+#     img_buffer = BytesIO()
+#     plt.savefig(img_buffer, format='png')
+#     img_buffer.seek(0)
+
+#     base64_encoded_graph = base64.b64encode(img_buffer.getvalue()).decode()
+#     return base64_encoded_graph
+
+from fastapi import FastAPI, HTTPException, Body
 import pandas as pd
-import matplotlib.pyplot as plt
 import base64
 from io import BytesIO
+import matplotlib.pyplot as plt
+from contextlib import redirect_stdout
+import io
 
 app = FastAPI()
 
 @app.post("/process_excel/")
-async def process_excel(url: str):
-    # Download the Excel file
+async def process_excel(url: str, python_script: str = Body(...)):
     try:
-        response = requests.get(url)
-        response.raise_for_status()  # This will raise an error for bad responses
-    except requests.RequestException as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        # Download the Excel file
+        df = pd.read_excel(url)
 
-    # Read the Excel file into a DataFrame
-    df = pd.read_excel(BytesIO(response.content))
+        # Redirect print statements to a variable
+        print_output = io.StringIO()
+        with redirect_stdout(print_output):
 
-    # Generate a graph and convert it to base64
-    graph_base64 = generate_graph_base64(df)
+            # Execute the Python script
+            # The script should use 'df' to access the dataframe
+            # and should return a matplotlib figure
+            local_vars = {}
+            exec(python_script, {'df': df, 'plt': plt}, local_vars)
+            fig = local_vars.get('fig')
 
-    # Return 'Hello World' and the graph
-    return {"message": "Hello World", "graph": graph_base64}
+            if not fig:
+                raise ValueError("No matplotlib figure found in the executed script.")
 
-def generate_graph_base64(dataframe):
-    plt.figure()
-    dataframe.plot(kind='line')  # Customize this as per your data
-    plt.tight_layout()
+            # Convert the figure to base64
+            buf = BytesIO()
+            fig.savefig(buf, format='png', bbox_inches='tight')
+            buf.seek(0)
+            graph_base64 = base64.b64encode(buf.read()).decode()
 
-    img_buffer = BytesIO()
-    plt.savefig(img_buffer, format='png')
-    img_buffer.seek(0)
-
-    base64_encoded_graph = base64.b64encode(img_buffer.getvalue()).decode()
-    return base64_encoded_graph
+        return {
+            "message": print_output.getvalue(),
+            "graph": graph_base64
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
